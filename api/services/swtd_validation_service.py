86d114e3b9ce1c63ce1ea6c14184381f7655db64
cxrvxxxx <brentempasisx3@gmail.com>
2024-05-08 21:19:57 +0800

    fix: Adjust indentation in SWTDValidationService class.

--- api/services/swtd_validation_service.py
+++ api/services/swtd_validation_service.py
@@ -7,37 +7,37 @@ class SWTDValidatioNService:
         self.db = db
         self.ft_service = ft_service
 
-def create_validation(self, swtd, proof):
-    validation = SWTDValidation(
-        swtd_id=swtd.id,
-        proof=proof
-    )
+    def create_validation(self, swtd, proof):
+        validation = SWTDValidation(
+            swtd_id=swtd.id,
+            proof=proof
+        )
 
-    self.db.session.add(validation)
-    self.db.session.commit()
+        self.db.session.add(validation)
+        self.db.session.commit()
 
-def update_validation(self, swtd, user, valid=None):
-    validation = swtd.validation
+    def update_validation(self, swtd, user, valid=None):
+        validation = swtd.validation
 
-    if valid == True:
-        validation.status = "APPROVED"
-        validation.validator = user
-        validation.validated_on = datetime.now()
-    elif valid == False:
-        validation.status = "REJECTED"
-        validation.validator = user
-        validation.validated_on = datetime.now()
-    else:
-        validation.status = "PENDING"
-        validation.validator = None
-        validation.validated_on = None
+        if valid == True:
+            validation.status = "APPROVED"
+            validation.validator = user
+            validation.validated_on = datetime.now()
+        elif valid == False:
+            validation.status = "REJECTED"
+            validation.validator = user
+            validation.validated_on = datetime.now()
+        else:
+            validation.status = "PENDING"
+            validation.validator = None
+            validation.validated_on = None
 
-    self.db.session.commit()
+        self.db.session.commit()
 
-def update_proof(self, swtd, file):
-    validation = swtd.validation
+    def update_proof(self, swtd, file):
+        validation = swtd.validation
 
-    self.ft_service.delete(swtd.author_id, swtd.id)
+        self.ft_service.delete(swtd.author_id, swtd.id)
 
-    validation.proof = file.filename
-    self.db.session.commit()
+        validation.proof = file.filename
+        self.db.session.commit()
