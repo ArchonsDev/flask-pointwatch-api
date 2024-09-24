0c4ed446abc432b800fc7c98b60007fa93afa87f
cxrvxxxx <brentempasisx3@gmail.com>
2024-09-24 23:18:40 +0800

    feat: add support for multi-date forms and increased benefit limit to 2k charas

--- api/models/swtd_form.py
+++ api/models/swtd_form.py
@@ -11,11 +11,10 @@ class SWTDForm(db.Model):
     venue = db.Column(db.String(255), nullable=False)
     category = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    time_started = db.Column(db.Time, nullable=False)
-    time_finished = db.Column(db.Time, nullable=False)
+    dates = db.Column(db.String(255), nullable=False)
     points = db.Column(db.Float, nullable=False)
-    benefits = db.Column(db.String(255), nullable=False)
+    benefits = db.Column(db.String(2000), nullable=False)
+    has_deliverables = db.Column(db.Boolean, nullable=False)
     is_deleted = db.Column(db.Boolean, nullable=False, default=False)
     term_id = db.Column(db.Integer, db.ForeignKey('tblterms.id'), nullable=False)
     # Link to SWTDValidation
@@ -31,12 +30,39 @@ class SWTDForm(db.Model):
             "venue": self.venue,
             "category": self.category,
             "role": self.role,
-            "date": self.date.strftime('%m-%d-%Y'),  # Convert date to string
-            "time_started":  self.time_started.strftime('%H:%M'),  # Convert time to string
-            "time_finished": self.time_finished.strftime('%H:%M'),  # Convert time to string
+            "dates": self.dates_to_json(),
             "points": self.points,
             "benefits": self.benefits,
+            "has_deliverables": self.has_deliverables,
             "is_deleted": self.is_deleted,
             "validation": self.validation.to_dict() if self.validation else None,
             "term": self.term.to_dict()
         }
+
+    @staticmethod
+    def dates_to_str(dates) -> str:
+        date_str = "".join(
+            f"{date.get("date")} {date.get("time_started")}-{date.get("time_ended")},"
+            for date in dates
+        )
+
+        return date_str[:-1]
+
+    def dates_to_json(self) -> dict[str, str]:
+        dateset = []
+
+        dates = self.dates.split(",")
+        for d in dates:
+            date, time = d.split(' ')
+
+            time_started, time_ended = time.split('-')
+
+            dateset.append(
+                {
+                    "date": date,
+                    "time_started": time_started,
+                    "time_ended": time_ended
+                }
+            )
+
+        return dateset
