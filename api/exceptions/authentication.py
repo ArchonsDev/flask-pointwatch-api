50ac20f4bbf7ec09778a78d3b3350dfd799af4d1
cxrvxxxx <brentempasisx3@gmail.com>
2024-10-24 08:16:14 +0800

    fix update status code for authentication error

--- api/exceptions/authentication.py
+++ api/exceptions/authentication.py
@@ -1,5 +1,5 @@
 from .base import APIError
 
 class AuthenticationError(APIError):
-    def __init__(self, message="Authentication failed.", status_code=403):
+    def __init__(self, message="Authentication failed.", status_code=401):
         super().__init__(message, status_code)
